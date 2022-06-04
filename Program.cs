e5c4ad5f5ddc750329d4aa0b7ecdd88a10262842
tienanh3011 <106862092+tienanh3011@users.noreply.github.com>
2022-06-04 17:08:19 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+namespace ConsoleApp3
+{
+    internal class Chuongtrinh
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ten gi: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Nam sinh: ");
+            int namsinh = Convert.ToInt32(Console.ReadLine());
+            int tuoi = 2022 - namsinh;
+            Console.WriteLine("Hello " + name + ", Nam nay ban duoc " + tuoi);
+            
+            
+
+           
+        }
+    }
+}
