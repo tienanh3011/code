8c24d953dc26c3fa057005fd929fff15cb8ba500
tienanh3011 <106862092+tienanh3011@users.noreply.github.com>
2022-06-25 17:07:03 +0700

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,21 +1,133 @@
-﻿// See https://aka.ms/new-console-template for more information
-using System;
-namespace ConsoleApp3
-{
-    internal class Chuongtrinh
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Ten gi: ");
-            string name = Console.ReadLine();
-            Console.WriteLine("Nam sinh: ");
-            int namsinh = Convert.ToInt32(Console.ReadLine());
-            int tuoi = 2022 - namsinh;
-            Console.WriteLine("Hello " + name + ", Nam nay ban duoc " + tuoi);
-            
-            //1132
+﻿using System;
+using System.Collections.Generic;
 
-           
+/* readkey
+ * key info
+ * background()
+ * Console.ForegroundColor
+ */
+
+namespace Tetris
+{
+    internal class xepgach
+    {
+        class Cell
+        {
+            private int[,] cell;
+            private ConsoleColor bg, fg;
+            private int x, y;
+
+            public Cell(ConsoleColor bg,ConsoleColor fg)
+            {
+                cell = new int[3, 3];
+                x = 0;
+                y = 0;
+                this.bg = bg;
+                this.fg = fg;
+                cell = NextCell();
+            }
+            private int[,] NextCell()
+            {
+                int maxCell = 6;
+                Random rnd=new Random();
+                return GetCell(rnd.Next(maxCell) + 1);
+
+            }
+            private int[,] GetCell(int idx)
+            {
+                switch(idx)
+                {
+                    case 1: return DefaultCell1(); break;
+                    case 2: return DefaultCell2(); break;
+                    case 3: return DefaultCell3(); break;
+                    case 4: return DefaultCell4(); break;
+                    case 5: return DefaultCell5(); break;
+                    
+                }
+                return DefaultCell6();
+            }
+            private int[,]DefaultCell1()
+            {
+                int[,] cell = { { 1, 1, 1 }, { 1, 1, 1 }, { 1, 1, 1 } };
+                return cell;
+            }
+            private int[,] DefaultCell2()
+            {
+                int[,] cell = { { 1, 1, 1 }, { 0, 1, 0 }, { 0, 0, 0 } };
+                return cell;
+            }
+            private int[,] DefaultCell3()
+            {
+                int[,] cell = { { 1, 0, 0 }, { 0, 0, 0 }, { 0, 0, 0 } };
+                return cell;
+            }
+            private int[,] DefaultCell4()
+            {
+                int[,] cell = { { 1, 1, 1 }, { 0, 0, 0 }, { 0, 0, 0 } };
+                return cell;
+            }
+            private int[,] DefaultCell5()
+            {
+                int[,] cell = { { 1, 1, 0 }, { 0, 1, 0 }, { 0, 1, 1 } };
+                return cell;
+            }
+            private int[,] DefaultCell6()
+            {
+                int[,] cell = { { 1, 0, 0 }, { 1, 0, 0 }, { 1, 1, 1 } };
+                return cell;
+            }
+            public void Draw()
+            {
+
+            }
+            public void Clear()
+            {
+
+            }
+            public void Move()
+            {
+
+            }
         }
+
+        static void Main (string[] args)
+      {
+        Console.ForegroundColor = ConsoleColor.Green;//doi mau chu
+        
+            int x=0, y=0;
+            int x_old, y_old;
+            Console.SetCursorPosition(x, y);//toa do
+            Console.WriteLine("***");
+            Console.WriteLine("***");
+            Console.WriteLine("***");
+            Console.BackgroundColor = ConsoleColor.Black;
+         do
+            {
+                x_old = x; y_old = y;
+                ConsoleKeyInfo banphim;
+                banphim=Console.ReadKey();//gan nut
+                if (banphim.Key == ConsoleKey.Escape) //escape
+                break;
+                    switch (banphim.Key)
+                    {
+                        case ConsoleKey.LeftArrow: x--; break;
+                        case ConsoleKey.RightArrow: x++; break;
+                        case ConsoleKey.UpArrow: y--; break;
+                        case ConsoleKey.DownArrow: y += 2; break; 
+                    }
+                Console.ForegroundColor = ConsoleColor.Black;
+                Console.SetCursorPosition(x_old, y_old);
+                Console.WriteLine("***");
+                Console.WriteLine("***");
+                Console.WriteLine("***");
+                Console.ForegroundColor = ConsoleColor.Green;//doi mau chu
+                Console.SetCursorPosition(x, y);
+                Console.WriteLine("***");
+                Console.WriteLine("***");
+                Console.WriteLine("***");
+
+            } while (true);
+            
+      }
     }
-}
+}      
